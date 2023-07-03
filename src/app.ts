ca4caa9cfe4f36271e7a03e1dbb7f71fff826fcd
Garegin Hakobyan <garik.hakobyan.2004@gmail.com>
2023-07-03 17:22:51 +0400

    add validation namespace

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,9 @@
 /// <reference path="drag-drop.ts"/>
 /// <reference path="project-model.ts"/>
 /// <reference path="project-state.ts"/>
+/// <reference path="validation.ts"/>
 
 namespace App {
-  // Validate
-  interface Validatable {
-    value: string | number;
-    required?: boolean;
-    minLength?: number;
-    maxLength?: number;
-    min?: number;
-    max?: number;
-  }
-
-  function validate(validateInput: Validatable) {
-    let isValid = true
-    const len = validateInput.value.toString().trim().length
-
-    if (validateInput.required) {
-      isValid = isValid && len !== 0
-    }
-
-    if (validateInput.minLength != null && typeof validateInput.value === "string") {
-      isValid = isValid && len >= validateInput.minLength
-    }
-
-    if (validateInput.maxLength != null && typeof validateInput.value === "string") {
-      isValid = isValid && len <= validateInput.maxLength
-    }
-
-    if (validateInput.min != null && typeof validateInput.value === "number") {
-      isValid = isValid && validateInput.value >= validateInput.min
-    }
-
-    if (validateInput.max != null && typeof validateInput.value === "number") {
-      isValid = isValid && validateInput.value <= validateInput.max
-    }
-
-    return isValid
-  }
-
   // Autobind
   function autobind(
     _: any,
---
+++ src/validation.ts
@@ -0,0 +1,39 @@
+namespace App {
+  // Validate
+  export interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+  }
+
+  export function validate(validateInput: Validatable) {
+    let isValid = true
+    const len = validateInput.value.toString().trim().length
+
+    if (validateInput.required) {
+      isValid = isValid && len !== 0
+    }
+
+    if (validateInput.minLength != null && typeof validateInput.value === "string") {
+      isValid = isValid && len >= validateInput.minLength
+    }
+
+    if (validateInput.maxLength != null && typeof validateInput.value === "string") {
+      isValid = isValid && len <= validateInput.maxLength
+    }
+
+    if (validateInput.min != null && typeof validateInput.value === "number") {
+      isValid = isValid && validateInput.value >= validateInput.min
+    }
+
+    if (validateInput.max != null && typeof validateInput.value === "number") {
+      isValid = isValid && validateInput.value <= validateInput.max
+    }
+
+    return isValid
+  }
+
+}
