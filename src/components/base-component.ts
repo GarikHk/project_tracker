0bb2a7128c080eb6cb7633318013efe7930a5ae4
Garegin Hakobyan <garik.hakobyan.2004@gmail.com>
2023-07-03 18:11:02 +0400

    implement es6 imp/exp in project item

--- src/components/base-component.ts
+++ src/components/base-component.ts
@@ -1,33 +1,31 @@
-namespace App {
-  export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
-    templateElement: HTMLTemplateElement
-    hostElement: T
-    element: U
+export default abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement
+  hostElement: T
+  element: U
 
-    constructor(
-      templateId: string,
-      hostElementId: string,
-      instertAtStart: boolean,
-      newElementId?: string
-    ) {
-      this.templateElement = document.getElementById(templateId) as HTMLTemplateElement
-      this.hostElement = document.getElementById(hostElementId) as T
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    instertAtStart: boolean,
+    newElementId?: string
+  ) {
+    this.templateElement = document.getElementById(templateId) as HTMLTemplateElement
+    this.hostElement = document.getElementById(hostElementId) as T
 
-      const importedNode = document.importNode(
-        this.templateElement.content,
-        true
-      )
-      this.element = importedNode.firstElementChild as U
-      if (newElementId) this.element.id = newElementId
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    )
+    this.element = importedNode.firstElementChild as U
+    if (newElementId) this.element.id = newElementId
 
-      this.attach(instertAtStart)
-    }
-
-    private attach(insert: boolean) {
-      this.hostElement.insertAdjacentElement(insert ? "afterbegin" : "beforeend", this.element)
-    }
-
-    abstract configure(): void
-    abstract renderContent(): void
+    this.attach(instertAtStart)
   }
+
+  private attach(insert: boolean) {
+    this.hostElement.insertAdjacentElement(insert ? "afterbegin" : "beforeend", this.element)
+  }
+
+  abstract configure(): void
+  abstract renderContent(): void
 }
--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,19 +1,16 @@
-namespace App {
-  // Autobind
-  export function autobind(
-    _: any,
-    _2: string,
-    descriptor: PropertyDescriptor
-  ) {
-    const originalMethod = descriptor.value
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const bound = originalMethod.bind(this)
-        return bound
-      }
+export function autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const bound = originalMethod.bind(this)
+      return bound
     }
-
-    return adjDescriptor
   }
+
+  return adjDescriptor
 }
