a66c33e47a0d3465728239f2e235be76530f86c1
Garegin Hakobyan <garik.hakobyan.2004@gmail.com>
2023-07-03 17:10:02 +0400

    add project model namespace

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,7 @@
 /// <reference path="drag-drop.ts"/>
+/// <reference path="project-model.ts"/>
 
 namespace App {
-  // PROJECT TYPE
-  enum ProjectStatus {
-    Active,
-    Finished
-  }
-
-  class Project {
-    constructor(
-      public id: string,
-      public title: string,
-      public description: string,
-      public people: number,
-      public status: ProjectStatus
-    ) {
-
-    }
-  }
-
   // PROJECT STATE MANAGMENT
   type Listener<T> = (items: T[]) => void
 
