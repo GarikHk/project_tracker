61426f30c3ce634380bd8357e5f7c15ad8a376d8
Garegin Hakobyan <garik.hakobyan.2004@gmail.com>
2023-07-03 17:36:13 +0400

    add project item and list namespaces

---
+++ src/components/project-item.ts
@@ -0,0 +1,40 @@
+namespace App {
+  // ProjectItem Class
+  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+    private project: Project
+
+    get people() {
+      let count = this.project.people
+      return `${count + (count > 1 ? " people" : " person")} assigned`
+    }
+
+    constructor(hostId: string, project: Project) {
+      super("single-project", hostId, false, project.id)
+      this.project = project
+
+      this.configure()
+      this.renderContent()
+    }
+
+    @autobind
+    dragStartHandler(event: DragEvent): void {
+      event.dataTransfer!.setData("text/plain", this.project.id)
+      event.dataTransfer!.effectAllowed = "move"
+    }
+
+    dragEndHandler(_: DragEvent): void {
+      console.log("DragEnd")
+    }
+
+    configure(): void {
+      this.element.addEventListener("dragstart", this.dragStartHandler)
+      this.element.addEventListener("dragend", this.dragEndHandler)
+    }
+
+    renderContent(): void {
+      this.element.querySelector("h2")!.textContent = this.project.title
+      this.element.querySelector("h3")!.textContent = this.people
+      this.element.querySelector("p")!.textContent = this.project.description
+    }
+  }
+}
